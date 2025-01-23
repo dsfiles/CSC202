6f80d28d819a80d15892d42d4b05beefc8b4aac1
GLi <guangyanli5@gmail.com>
2025-01-22 19:02:14 -0800

    Add project files.

---
+++ CSC202/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CSC202
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(100));
+        }
+
+        public static int Sum(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n + Sum(n - 1);
+        }
+    }
+}
